0b16f77c008421890c58a19b11b866a51ef14c75
IYuBelov <iyubelov@gmail.com>
2026-06-27 11:42:11 +0200

    text style

--- src/common/word_condition.gd
+++ src/common/word_condition.gd
@@ -18,6 +18,8 @@ const MESSAGE_FONT_MIN := 56
 const PATTERN_HI_FONT_MAX := 140
 const PATTERN_HI_FONT_MIN := 72
 const PATTERN_DIM_RATIO := 0.72
+const PATTERN_DASH_NBSP_COUNT := 5
+const PATTERN_SIDE_NBSP_COUNT := 2
 
 
 static func random() -> int:
@@ -65,16 +67,34 @@ static func _build_message_bbcode(title: String, font_size: int) -> String:
 
 static func _build_pattern_bbcode(syllable: String, condition: int, hi_size: int, dim_size: int) -> String:
 	var hi := "[font_size=%d][color=#FFFFFF]%s[/color][/font_size]" % [hi_size, syllable]
-	var dim := "[font_size=%d][color=#FFFFFF]%s[/color][/font_size]" % [dim_size, "%s"]
+	var dash := _dash_bbcode(dim_size)
+	var side := _side_dash_bbcode(dim_size)
 	match condition:
 		Type.BEGIN:
-			return hi + (dim % "___")
+			return hi + dash
 		Type.END:
-			return (dim % "___") + hi
+			return dash + hi
 		Type.ANYWHERE:
-			return (dim % "_") + hi + (dim % "_")
+			return side + hi + side
 		_:
 			return hi
+
+
+static func _dash_bbcode(dim_size: int) -> String:
+	return _underline_bbcode(dim_size, PATTERN_DASH_NBSP_COUNT)
+
+
+static func _side_dash_bbcode(dim_size: int) -> String:
+	return _underline_bbcode(dim_size, PATTERN_SIDE_NBSP_COUNT)
+
+
+static func _underline_bbcode(dim_size: int, nbsp_count: int) -> String:
+	var spaces := "\u00a0".repeat(nbsp_count)
+	return "[font_size=%d][color=#FFFFFF][u]%s[/u][/color][/font_size]" % [dim_size, spaces]
+
+
+static func _measure_dash_width(dim_size: int, nbsp_count: int) -> float:
+	return _measure_text_width("\u00a0".repeat(nbsp_count), dim_size)
 
 
 static func _measure_text_width(text: String, font_size: int) -> float:
@@ -85,10 +105,14 @@ static func _measure_text_width(text: String, font_size: int) -> float:
 static func _measure_pattern_width(syllable: String, condition: int, hi_size: int, dim_size: int) -> float:
 	match condition:
 		Type.BEGIN:
-			return _measure_text_width(syllable, hi_size) + _measure_text_width("___", dim_size)
+			return _measure_text_width(syllable, hi_size) + _measure_dash_width(dim_size, PATTERN_DASH_NBSP_COUNT)
 		Type.END:
-			return _measure_text_width("___", dim_size) + _measure_text_width(syllable, hi_size)
+			return _measure_dash_width(dim_size, PATTERN_DASH_NBSP_COUNT) + _measure_text_width(syllable, hi_size)
 		Type.ANYWHERE:
-			return _measure_text_width("_", dim_size) + _measure_text_width(syllable, hi_size) + _measure_text_width("_", dim_size)
+			return (
+				_measure_dash_width(dim_size, PATTERN_SIDE_NBSP_COUNT)
+				+ _measure_text_width(syllable, hi_size)
+				+ _measure_dash_width(dim_size, PATTERN_SIDE_NBSP_COUNT)
+			)
 		_:
 			return _measure_text_width(syllable, hi_size)
